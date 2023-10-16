bd59ecfe721d75a121de92fa20243e7f0a0e55a6
Jochen Scheib <webmaster@mapero.de>
2023-10-16 23:57:14 +0200

    a bit more functional code

--- components/hcpbridge/hcpbridge.cpp
+++ components/hcpbridge/hcpbridge.cpp
@@ -6,15 +6,30 @@ namespace hcpbridge {
 
 cover::CoverTraits HCPBridgeComponent::get_traits() {
     auto traits = cover::CoverTraits();
-    traits.set_is_assumed_state(false);
-    traits.set_supports_position(false);
+    traits.set_is_assumed_state(true);
+    traits.set_supports_position(true);
     traits.set_supports_tilt(false);
 
     return traits;
 }
 
 void HCPBridgeComponent::control(const cover::CoverCall &call) {
-    
+  if(!HoermannGarageEngine::getInstance().state->valid) {
+    ESP_LOGD(TAG, "Not valid");
+    return;
+  }
+  
+  if (call.get_stop()) {
+    ESP_LOGD(TAG, "Stop");
+    HoermannGarageEngine::getInstance().stopDoor();
+    this->publish_state();
+  } else if (call.get_position().has_value()) {
+    ESP_LOGD(TAG, "Open");
+    HoermannGarageEngine::getInstance().setPosition(call.get_position().value());
+  } else {
+    ESP_LOGD(TAG, "Unknown command");
+  }
+
 }
 
 void HCPBridgeComponent::setup() {
