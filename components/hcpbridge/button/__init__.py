d61fd81ade4ff85d9d62da9f80ef9c1e762fab6e
MCLW <maik.cervntes@gmx.de>
2024-04-17 11:05:44 +0200

    bugfix: define HCPBridgeButton in hcpbrige_ns

--- components/hcpbridge/button/__init__.py
+++ components/hcpbridge/button/__init__.py
@@ -6,7 +6,7 @@ from .. import hcpbridge_ns, CONF_HCPBridge_ID, HCPBridge
 
 DEPENDENCIES = ["hcpbridge"]
 
-HCPBridgeButton = hcpbridge_ns.class_("HCPBridgeSwitch", button.Button, cg.Component)
+HCPBridgeButton = hcpbridge_ns.class_("HCPBridgeButton", button.Button, cg.Component)
 
 CONFIG_SCHEMA = (
     button.button_schema(HCPBridgeButton, icon=ICON_FAN)
