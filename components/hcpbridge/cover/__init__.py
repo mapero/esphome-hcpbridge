0251a0d35084cc9c85cb5363feb2c5f236f9b0cd
Steffen Pankratz <kratz00@gmx.de>
2025-05-28 16:12:11 +0200

    Fix: WARNING Using `cover.COVER_SCHEMA` is deprecated
    Signed-off-by: Steffen Pankratz <kratz00@gmx.de>

--- components/hcpbridge/cover/__init__.py
+++ components/hcpbridge/cover/__init__.py
@@ -12,10 +12,12 @@ DEPENDENCIES = ['hcpbridge']
 HCPBridgeCover = hcpbridge_ns.class_("HCPBridgeCover", cover.Cover, cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.All(
-  cover.COVER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(HCPBridgeCover),
-    cv.GenerateID(CONF_HCPBridge_ID): cv.use_id(HCPBridge),
-  }).extend(cv.polling_component_schema("500ms")),
+  cover.cover_schema(HCPBridgeCover)
+  .extend(
+      {
+        cv.GenerateID(CONF_HCPBridge_ID): cv.use_id(HCPBridge),
+      }
+  ).extend(cv.polling_component_schema("500ms")),
 )
 
 async def to_code(config):
